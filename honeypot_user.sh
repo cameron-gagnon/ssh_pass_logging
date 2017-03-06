794dcb32f055906c57301d62cfc98387bec5a72a
Cameron Gagnon <cameron.gagnon@gmail.com>
2017-03-06 18:40:33 -0500

    don't try to create usernames > 32 chars. still store them in usernames.txt though

--- honeypot_user.sh
+++ honeypot_user.sh
@@ -4,7 +4,12 @@
 
 if [[ -n "$1" ]]
 then
-    ERROR_DATA=$( { useradd -c "honeypot user" -d /home/honeypot -g 2000 -m -o -s /bin/false -u 2000 $1; } 2>&1 )
+    if [[ ${#1} -gt 32 ]];
+    then
+        echo "Username too long: $1"
+        exit
+    fi
+    ERROR_DATA=$( { useradd -c "honeypot user" -d /home/honeypot -g 2000 -m -o -s /bin/false -u 2000 "$1"; } 2>&1 )
     ret_code=$?
     if [[ $ret_code = 6 ]];
     then
